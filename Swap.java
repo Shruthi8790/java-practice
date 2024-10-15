8fdede98384efb9c265ddf0a3cd26e71859d9eb6
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:26:12 +0530

    Swap.java

--- Swap.java
+++ Swap.java
@@ -1,11 +1,12 @@
 package work;
-
+//Taking a class name as Swap
 public class Swap {
 
 	public static void main(String[] args) {
+		//Member Variables
 		int a=67;
 		int b=34;
-		//b=b-a;
+		// Swapping the values
 		a=a+b;
 		b=a-b;
 		a=a-b;
