405524c57fd567dac8a679a318012480967589d1
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:16:43 +0530

    ScannerDemo.java

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class ScannerDemo {
 
 	public static void main(String[] args) {
+		//Creating object for Scanner class
 		Scanner sc=new Scanner(System.in);
+		// taking input through keyboard
 		System.out.println("Enter a number:");
 		int age=sc.nextInt();
 		System.out.println("Enter a name");
@@ -10,6 +12,7 @@ public class ScannerDemo {
 		//sc.next();
 		System.out.println("Enter a father name");
 		String fname=sc.next();
+		// Displaying the data on console
 		System.out.println("My Age is:" +age);
 		System.out.println("My Name is:" +name);
 		System.out.println("My Father Name is:" +fname );
