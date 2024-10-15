fdb2bb81cc5a770c70fd4fe41f2f07d46c4a2739
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:05:08 +0530

    Continue.java

--- Continue.java
+++ Continue.java
@@ -2,22 +2,16 @@ package work;
 
 public class Continue 
 {
-
 public static void main(String[] args) 
 {
-		
 int i;
 for(i=0;i<=10;i++)
-		{
-			if(i==5) {
-				break;
-				}
-				
-				System.out.println(i);
-			
-		
-	}
-
-}
+{
+  if(i==5) {
+     break;
+    }
+System.out.println(i);
+   }
+  }
 }
 
