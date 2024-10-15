4ad0bf78a48f662187e8c8c8409684146589611b
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:14:10 +0530

    OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class OddOrEven {
 
 	public static void main(String[] args) {
+	// creating a object for Scanner class
 	Scanner sc=new Scanner(System.in);
+	//Taking input through keyboard
 	System.out.println("enter a number");
 	int a=sc.nextInt();
 	if(a%2==0)
