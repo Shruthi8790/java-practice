8bddf3fbd0f738fc52dcca45ffbc87c2af086ce9
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:02:35 +0530

    ArrayExample1.java

--- ArrayExample1.java
+++ ArrayExample1.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class ArrayExample1 {
 
 	public static void main(String[] args) {
+        // member variables
 	int[] numbers;
 	numbers=new int[15];
 	int index;
+        // Creating object for Scanner class
 	Scanner sc=new Scanner(System.in);
+	//Taking input through keyboard
 	System.out.println("Enter any 15 number:");
 	for(index=0;index<numbers.length;index++)
 	{
