c156407ed7a269cf2ba46fea4f03e205f93b3c05
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:18:47 +0530

    ComputerInfo.java

--- SecondProgram.java
+++ SecondProgram.java
@@ -1,6 +1,6 @@
 package secondproject;
 
-public class SecondProgram {
+public class computerInfo {
 
 	public static void main(String[] args) {
 		System.out.println("computer is a electronic device");
@@ -8,7 +8,7 @@ public class SecondProgram {
 		System.out.println("it is invented by charles bage");
 		System.out.println("he also known as father of computer ");
 		System.out.println("it is understand only binary language");
-		System.out.println("binary lan reffered as 0's and 1's");
+		System.out.println("binary language reffered as 0's and 1's");
 		System.out.println("to complete course practice");
 		System.out.println("to create the test material");
 		System.out.println("to plane and shcedule");
