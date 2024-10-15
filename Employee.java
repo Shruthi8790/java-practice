9f8335a149d4f1b6e6be6a3a263d109a7a87e23e
Shruthi8790 <pandemshruthireddy@gmail.com>
2024-10-15 18:07:56 +0530

    Employee.java

--- Employee.java
+++ Employee.java
@@ -1,11 +1,13 @@
-
+// Taking a class name as Employee
 public class Employee {
+	//Member variables
 	protected int EmpId;
 	 protected int EmpAge;
 	 protected String Empname;
 	 protected boolean isPermanent;
 
 	public static void main(String[] args) {
+		// creating a object for Employee class
 		Employee emp=new Employee();
 		emp.EmpAge=(int)35.5;
 		System.out.println("Successfully started");
